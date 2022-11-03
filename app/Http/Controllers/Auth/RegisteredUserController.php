c318e318970c2b0d9ee23374bd7f61c032bb5356
patry <patryk.gajewski@pollub.edu.pl>
2022-11-03 18:53:55 +0100

    login/register with auth and alerts

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Hamcrest\Core\AllOf;
+use Illuminate\Auth\Events\Registered;
+use RealRashid\SweetAlert\Facades\Alert;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Handle an incoming registration request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+        try {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            event(new Registered($user));
+            Alert::success("Congratulation", "User registered successfully");
+        } catch (\Exception $e) {
+            Alert::error("Something was wrong", $e->getMessage());
+            return redirect(RouteServiceProvider::REGISTER);
+        }
+        return redirect(RouteServiceProvider::LOGIN);
+    }
+
+    /**
+     * Display the registration view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('auth.register');
+    }
+}
